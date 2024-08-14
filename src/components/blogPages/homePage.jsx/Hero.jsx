1deff6d8809c829300b489d898a8fe82d317c4e0
KeswetRinret <rkeswety@gmail.com>
2024-08-13 22:00:15 -0700

    corrections

--- src/components/blogPages/homePage.jsx/Hero.jsx
+++ src/components/blogPages/homePage.jsx/Hero.jsx
@@ -22,25 +22,25 @@ const Hero = () => {
       <h1 className="md:text-3xl text-2xl font-bold text-blue-950 text-center mt-2 mb-8">
         Welcome to Our Blog
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {blogs && blogs.map((blog, index) => (
           <div 
             key={index} 
-            className="flex flex-col bg-gray-100 px-3 py-2 rounded-lg shadow-lg"
+            className="flex flex-col bg-gray-100 p-6 rounded-lg shadow-lg"
           >
             <img 
               src={blog.urlToImage} 
-              alt={blog.title} 
-              className="w-full h-64 object-cover rounded-lg mb-4" 
+              alt=""
+              className="w-full h-40 object-cover rounded-lg mb-4" 
             />
             <h2 className="text-xl font-semibold text-blue-900 mb-2">
               {blog.title}
             </h2>
-            <p className="text-gray-700 mb-4">
+            <p className="text-gray-700 mb-2">
               {blog.description || "No description available"}
             </p>
             <button 
-              className="bg-blue-400 text-black font-medium py-2 px-4 rounded hover:bg-blue-500 transition duration-200 self-start flex justify-end"
+              className="bg-blue-400 text-black font-medium py-2 px-4 rounded-xl hover:bg-blue-500 transition duration-200 self-end"
             >
               Read More
             </button>
